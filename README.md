a8fc7de458ce494868d71fddeeb42506b0b2ec4d
Brandon Thomas <echelon@gmail.com>
2016-12-14 02:10:37 -0500

    Fix the DAC API. I had left it unfinished.
    Also update README and a few other things.

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 etherdream.rs
 =============
+A library for the [EtherDream laser DAC](http://ether-dream.com/).
 
-**Work in progress**
-
-A library for the Etherdream laser projector DAC.
+**This is an early work in progress (2016-12-14)**. Things work, but use
+at your own risk. The API hasn't been thought out, and there is no error
+handling (a hallmark of any decent Rust project). I do intend to finish
+a production-ready version of the library within a month or two.
 
 See also
 --------
-
 I'm beginning to build out Rust libraries and tools for laser
 projection:
 
 - [Etherdream Emulator](https://github.com/echelon/etherdream-emulator),
-  a visual emulator for the Etherdream laser projector DAC
+  a visual emulator for the EtherDream laser projector DAC
 
 - [ILDA.rs](https://github.com/echelon/ilda.rs), a Rust parser for the
   ILDA laser display format.
@@ -44,7 +45,6 @@ EtherDream:
 
 License
 -------
-
 **BSD 4-clause**
 
 Copyright (c) 2016, Brandon Thomas. All rights reserved.
@@ -81,4 +81,3 @@ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
-
--- examples/draw_circle.rs
+++ examples/draw_circle.rs
@@ -1,18 +1,13 @@
 // Copyright (c) 2016 Brandon Thomas <bt@brand.io>, <echelon@gmail.com>
 // Etherdream.rs, a library for the EtherDream laser projector DAC.
 
-extern crate byteorder;
 extern crate etherdream;
 
-use byteorder::LittleEndian;
-use byteorder::WriteBytesExt;
 use etherdream::dac::Dac;
 use etherdream::protocol::Point;
 use std::f64::consts::PI;
 use std::f64;
 
-// TODO TEMP: This is just lazy.
-static mut J : i32 = 0;
 static DIV : i32 = 200;
 
 fn main() {
@@ -31,32 +26,25 @@ fn main() {
   };
 
   let mut dac = Dac::new(ip_addr);
-  //dac.play_demo();
+
+  static mut pos: i32 = 0;
 
   dac.play_function(|num_points: u16| {
-    let mut cmd : Vec<u8> = Vec::new();
-    cmd.push(0x64); // 'data' command.
-    // TODO/FIXME: This should be LittleEndian. Why does this work only
-    // as BigEndian!?
-    cmd.write_u16::<LittleEndian>(num_points).unwrap();
-
+    let mut points = Vec::new();
     for _i in 0 .. num_points {
-      // TODO TEMP
       let f = unsafe {
-        J = (J + 1) % DIV;
-        J
+        pos = (pos + 1) % DIV;
+        pos
       };
 
-      //let m = i as f64 * 1.0;
       let j = (f as f64 / DIV as f64) * 2 as f64 * PI;
       let x = j.cos() * 100.0f64;
       let y = j.sin() * 100.0f64;
-      let pt = Point::xy_rgb(x as i16, y as i16, 255, 255, 255);
-      //let pt = Point::xy_rgb(0, 0, 255, 255, 255);
-      cmd.extend(pt.serialize());
+
+      points.push(Point::xy_rgb(x as i16, y as i16, 255, 255, 255));
     }
 
-    cmd
+    points
   });
 }
 
